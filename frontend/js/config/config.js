56b5b59706b4a6a62ccc833755faf8fff76f8856
Casey Flynn <caseyflynn@me.com>
2016-07-27 14:43:29 -0400

    Background speed.

--- frontend/js/config/config.js
+++ frontend/js/config/config.js
@@ -4,7 +4,7 @@ var FlappyBird = FlappyBird || {};
 
 FlappyBird.GRAVITY = 0.6;
 FlappyBird.SCROLL_SPEED = 2;
-FlappyBird.BACKGROUND_SPEED = 2;
+FlappyBird.BACKGROUND_SPEED = 1;
 FlappyBird.PIPE_GAP = 150;
 FlappyBird.PIPE_SPACING = 200;
 FlappyBird.MIN_PIPE_Y = 150;
